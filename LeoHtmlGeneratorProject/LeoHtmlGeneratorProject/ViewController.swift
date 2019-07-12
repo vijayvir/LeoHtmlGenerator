40e1e0565e582bee2cc737074acc618b94bdf7d4
vijayvir <virvijay37@gmail.com>
2019-07-12 12:03:22 +0530

    t
    t

--- LeoHtmlGeneratorProject/LeoHtmlGeneratorProject/ViewController.swift
+++ LeoHtmlGeneratorProject/LeoHtmlGeneratorProject/ViewController.swift
@@ -42,8 +42,63 @@ class ViewController: UIViewController {
         var nodesd : [Node] = []
         
         
+        var rows : [ChildOf<Tag.Table>] = []
+        
+        for index in 1 ... 6 {
+            
+            let row  :ChildOf<Tag.Table> =  .tr(.th("1"),
+                                                 .th("2"),
+                                                 .th("4"),
+                                                 . th("4"),
+                                                 .th("4t"),
+                                                 .th("5"),   .th("5")
+                                          
+                
+                
+                
+            )
+          rows.append(row)
+            
+        }
+        
+        
+        let table : Node  = Node.table( attributes: [.border(1) ,
+                                                     .align(.left)  ,  .style(safe: "border: 1px solid red;   border-collapse: collapse; ")],
+          .tr(.th("Category"),
+              .th("Units"),
+              .th("Rate per Units"),
+             . th("Weight per Units(Qt)"),
+             .th("Total Weight"),
+             .th("Rate per Units"),
+             . th("Weight per Units(Qt)"),
+             .th("Total Weight"),
+             .th("Rate per Units"),
+             . th("Weight per Units(Qt)"),
+             .th("Total Weight"),
+             .th("Rate per Units"),
+             .th("Total Weight"),
+             .th("Rate per Units"),
+             . th("Weight per Units(Qt)"),
+             .th("Total Weight"),
+             .th("Rate per Units"),
+             . th("Weight per Units(Qt)"),
+             .th("Total Weight"),
+             .th("Rate per Units"),
+             . th("Weight per Units(Qt)"),
+             .th("Total Weight"),
+            .th("Total Rate") ),
+            .fragment(rows),
+                     .fragment(rows)
+            
+        
+        
+        )
+        
         
         for object in someValue {
+            
+            
+            
             let listTag = Node.ul(
                 .li(  "index: \(object.index)"),
                 .li("Dog" , "\(object.value)"),
@@ -66,16 +121,43 @@ class ViewController: UIViewController {
         
         
         
-        
+        let some  = "fdasd"
         
         
         let document: Node = .document(
             .html(
                 .body(
-                     .b( "Hello" , .i("World") , "dsdd"  ),
-                    .h1("Welcome!"),
-                    .p("You’ve found our site!"),
-                    .fragment(nodesd)
+                    
+                    .p( "Made with Raji Farm App. "   ,  .b(  attributes : [ .style(safe: "float: right; ")], "\(Date().description)")  ),
+            
+                    
+                    .h1( "Title : "   ),
+                    .p("You’ve found our site!" ),
+                 
+                    
+
+                   .br,
+                   .br,
+                    .fragment(nodesd) ,
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),.div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                       .div(table),
+                          .p("You’ve found our site!")
                 )
             )
         )
@@ -94,21 +176,24 @@ class ViewController: UIViewController {
         let fmt = UIMarkupTextPrintFormatter(markupText: html)
         
         // 2. Assign print formatter to UIPrintPageRenderer
-        let render = UIPrintPageRenderer()
-        render.addPrintFormatter(fmt, startingAtPageAt: 0)
-        
+        let renderPAge = UIPrintPageRenderer()
+        renderPAge.addPrintFormatter(fmt, startingAtPageAt:0)
+        renderPAge.addPrintFormatter(self.view.viewPrintFormatter(), startingAtPageAt: 0)
         // 3. Assign paperRect and printableRect
         let page = CGRect(x: 0, y: 0, width: 595.2, height: 841.8) // A4, 72 dpi
-        render.setValue(page, forKey: "paperRect")
-        render.setValue(page, forKey: "printableRect")
+        renderPAge.setValue(page, forKey: "paperRect")
+        renderPAge.setValue(page, forKey: "printableRect")
         
         // 4. Create PDF context and draw
         let pdfData = NSMutableData()
         UIGraphicsBeginPDFContextToData(pdfData, .zero, nil)
         
-        for i in 0..<render.numberOfPages {
+        for i in 0..<renderPAge.numberOfPages {
             UIGraphicsBeginPDFPage();
-            render.drawPage(at: i, in: UIGraphicsGetPDFContextBounds())
+          //  renderPAge.drawHeaderForPage(at: i, in: CGRect(x: 0, y: 0, width: 595.2, height: 80))
+           //   drawHeaderForPage
+            
+            renderPAge.drawPage(at: i, in: UIGraphicsGetPDFContextBounds())
         }
         
         UIGraphicsEndPDFContext();
